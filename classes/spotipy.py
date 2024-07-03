b8f3a78defa0b66a68942001891ca6a44a9c115d
ItsMeGeorge <1georgepearson@gmail.com>
2024-07-03 19:08:28 +0100

    Passed .env file path

--- classes/spotipy.py
+++ classes/spotipy.py
@@ -96,7 +96,7 @@ class SpotipyAPI:
 
     def __init__(self):
         # Loads the enviroment variables
-        load_dotenv(dotenv_path="~/SpotiPi")
+        load_dotenv(dotenv_path="~/SpotiPi/.env")
 
         self.client_id = os.getenv("CLIENT_ID")
         self.client_secret = os.getenv("CLIENT_SECRET")
